93e4ac3be3bae04cab55c22309762548a60237c7
Sergio Fidelis <sergiofidelis31@hotmail.com>
2023-02-03 23:44:10 -0300

    Add cors config and swagger

--- src/main/java/com/shoppingcart/backend/requests/CategoryRequest.java
+++ src/main/java/com/shoppingcart/backend/requests/CategoryRequest.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.NotEmpty;
 
 @Data
 public class CategoryRequest {
-    @NotEmpty(message = "category id cannot be null")
     private Long id;
 
     @NotEmpty(message = "category name cannot be null")
